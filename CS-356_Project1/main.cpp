b477628b3f9e39ca6830f32766e78bfa41a75f0f
Pedro Quinones <pedrolquinones@gmail.com>
2014-02-12 18:22:20 -0700

    Initial Commit

---
+++ CS-356_Project1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS-356_Project1
+//
+//  Created by Pedro Quinones on 2/12/14.
+//  Copyright (c) 2014 Pedro Quinones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
